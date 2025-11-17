01bd988e210dfc36292992ee6d9981697a734528
nyatinte <takumi.062757@gmail.com>
2025-11-18 00:01:48 +0900

    add gtr config

--- home/dot_zsh.d/aliases.zsh
+++ home/dot_zsh.d/aliases.zsh
@@ -18,6 +18,19 @@ alias glog='git log --oneline --graph --decorate'
 alias gprw="gh pr view --web"
 alias grw="gh repo view --web"
 
+
+# Git worktree runner -- <https://github.com/coderabbitai/git-worktree-runner>
+gwn() {
+    local branch_name="$1"
+    if [ -z "$branch_name" ]; then
+        echo "Usage: gwn <branch-name>"
+        return 1
+    fi
+    local default_branch=$(gh api repos/:owner/:repo --jq .default_branch 2>/dev/null || echo "main")
+    # WHY g'w'r ? -> 'gtr' command conflicts. see <https://github.com/coderabbitai/git-worktree-runner/issues/11>
+    gwr new "$branch_name" --from "origin/$default_branch" && cd "$(gwr go "$branch_name")"
+}
+
 # システム
 alias reload='source ~/.zshrc'
 

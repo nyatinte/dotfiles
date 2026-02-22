0123d66e7232af46f552da8c978c26e354fdcf18
nyatinte <takumi.062757@gmail.com>
2026-02-22 22:52:44 +0900

    feat(ccstatusline): add underline style to repo URL

--- home/dot_config/ccstatusline/executable_repo-url.sh
+++ home/dot_config/ccstatusline/executable_repo-url.sh
@@ -9,4 +9,4 @@ repo_data=$(gh repo view --json url,nameWithOwner 2>/dev/null) || { echo "reposi
 repo_url=$(printf '%s' "$repo_data" | jq -r .url)
 name_with_owner=$(printf '%s' "$repo_data" | jq -r .nameWithOwner)
 
-printf '\033[96m\033]8;;%s\033\\%s%s\033]8;;\033\\\033[0m\n' "$repo_url" "$GITHUB_ICON" "$name_with_owner"
+printf '\033[96m\033[4m\033]8;;%s\033\\%s%s\033]8;;\033\\\033[0m\n' "$repo_url" "$GITHUB_ICON" "$name_with_owner"
